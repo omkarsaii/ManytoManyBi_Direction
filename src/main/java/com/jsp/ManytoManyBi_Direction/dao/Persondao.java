0c51e269020cbc0050e29095534ca1537e457687
Your Name <your_email@example.com>
2024-08-20 11:38:45 +0530

    added person dao

--- src/main/java/com/jsp/ManytoManyBi_Direction/dao/Persondao.java
+++ src/main/java/com/jsp/ManytoManyBi_Direction/dao/Persondao.java
@@ -1,5 +1,16 @@
 package com.jsp.ManytoManyBi_Direction.dao;
 
-public class Persondao {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+public class Persondao {
+	
+	public EntityManager getEntityManager()
+	{
+		EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("Omkarr");
+		EntityManager entityManager=entityManagerFactory.createEntityManager();
+		return entityManager;
+	}
+	
 }
