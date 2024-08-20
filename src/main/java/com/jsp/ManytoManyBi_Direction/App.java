9901f98d5b7f53970eada8dc1f01667d272ac13a
Your Name <your_email@example.com>
2024-08-20 12:34:48 +0530

    changes are made in main class

--- src/main/java/com/jsp/ManytoManyBi_Direction/App.java
+++ src/main/java/com/jsp/ManytoManyBi_Direction/App.java
@@ -1,13 +1,46 @@
 package com.jsp.ManytoManyBi_Direction;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jsp.ManytoManyBi_Direction.dao.LanguageDao;
+import com.jsp.ManytoManyBi_Direction.dao.Persondao;
+import com.jsp.ManytoManyBi_Direction.dta.Language;
+import com.jsp.ManytoManyBi_Direction.dta.Person;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	LanguageDao languageDao = new LanguageDao();
+       Persondao persondao=new Persondao();
+    	
+      Language english=new Language();
+      english.setName("English");
+      english.setOrigin("England");
+      
+      Language spanish = new Language();
+      spanish.setName("Spanish");
+      spanish.setOrigin("Spain");
+      
+      Person john = new Person();
+      john.setName("John Doe");
+      john.setAge(30);
+      
+      List<Language>list=new ArrayList<Language>();
+      list.add(english);
+      list.add(spanish);
+      john.setLanguages(list);
+      
+      List<Person> personsForEnglish = new ArrayList<>();
+      personsForEnglish.add(john);
+      english.setPersons(personsForEnglish);
+
+      List<Person> personsForSpanish = new ArrayList<>();
+      personsForSpanish.add(john);
+      spanish.setPersons(personsForSpanish);
+      
+      persondao.saveperson(john);
+      
     }
 }
