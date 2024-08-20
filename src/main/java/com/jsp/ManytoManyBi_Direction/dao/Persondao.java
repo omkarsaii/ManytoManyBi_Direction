316755671694c6209cd812bdddc9024fa16ef58c
Your Name <your_email@example.com>
2024-08-20 11:56:37 +0530

    made changes in Persondao class

--- src/main/java/com/jsp/ManytoManyBi_Direction/dao/Persondao.java
+++ src/main/java/com/jsp/ManytoManyBi_Direction/dao/Persondao.java
@@ -2,7 +2,10 @@ package com.jsp.ManytoManyBi_Direction.dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import com.jsp.ManytoManyBi_Direction.dta.Person;
 
 public class Persondao {
 	
@@ -13,4 +16,47 @@ public class Persondao {
 		return entityManager;
 	}
 	
+	public void saveperson(Person person)
+	{
+		EntityManager entityManager=getEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		
+		entityTransaction.begin();
+		entityManager.persist(person);
+		entityTransaction.commit();
+	}
+	
+	public void updateperson(int id,Person person)
+	{
+		EntityManager entityManager=getEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		
+		Person person1=entityManager.find(Person.class, id);
+		if(person1!=null)
+		{
+			person.setId(id);
+			person.setLanguages(person1.getLanguages());
+			entityTransaction.begin();
+            entityManager.merge(person);
+            entityTransaction.commit();
+		}
+		else
+		{
+			System.out.println("Person not found");
+		}
+	}
+	  public void deletePerson(int id) {
+	        EntityManager entityManager = getEntityManager();
+	        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+	        Person person = entityManager.find(Person.class, id);
+
+	        if (person != null) {
+	            entityTransaction.begin();
+	            entityManager.remove(person);
+	            entityTransaction.commit();
+	        } else {
+	            System.out.println("Person doesn't exist.");
+	        }
+	    }
 }
